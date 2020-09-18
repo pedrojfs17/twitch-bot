d5750326991fee8a56366c86de734ab12fe5ac39
Pedro Seixas <49153615+pedrojfs17@users.noreply.github.com>
2020-09-18 10:59:10 +0100

    Added ENV

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const CHANNEL_NAME = 'TVMarvin';
-export const OAUTH_TOKEN = 'oauth:1oi3k8vyozh7i53xrcnnamdcbl1l4z';
+export const OAUTH_TOKEN = process.env.OAUTH_TOKEN;
 export const BOT_USERNAME = 'BazingaBot';
