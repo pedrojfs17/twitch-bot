fa5ab1685b5d8a95486cca24081fe5631a8a5c29
Pedro Seixas <49153615+pedrojfs17@users.noreply.github.com>
2020-09-18 10:51:28 +0100

    Fisrt Commit

---
+++ src/app.js
@@ -0,0 +1,70 @@
+import tmi from "tmi.js"
+import {CHANNEL_NAME, OAUTH_TOKEN, BOT_USERNAME} from "./constants"
+
+const options = {
+	options: { debug: true },
+	connection: {
+		reconnect: true,
+		secure: true
+	},
+	identity: {
+		username: BOT_USERNAME,
+		password: OAUTH_TOKEN
+	},
+	channels: [ CHANNEL_NAME ]
+}
+
+const client = new tmi.Client(options);
+
+client.connect().catch(console.error);
+
+client.on('message', (channel, user, message, self) => {
+	if(self) return;
+
+	if ((message.indexOf('!')) !== -1) {
+		const command = recognizeCommand(message);
+
+		if (!command) return
+
+		callCommand(command, { channel, user, message });
+	}
+});
+
+let recognizeCommand = (message) => {
+	const regex = /\!(.*?)$/gm;
+	const fullCommand = regex.exec(message);
+  
+	if (fullCommand) {
+	  const splittedCommand = fullCommand[1].split(' ')
+	  const command = splittedCommand[0];
+  
+	  splittedCommand.shift() // remove command from array
+  
+	  return {
+		command: command,
+		args: splittedCommand
+	  }
+	}
+  
+	return false
+}
+
+function callCommand(command, messageInfo) {
+	switch (command.command) {
+	  case 'jogar':
+		playCommandHandler(command, messageInfo);
+		break
+	  default:
+		break
+	}
+}
+
+function playCommandHandler(command, messageInfo) {
+	// Add user to list with command.args[0] nickname
+	if (command.args.length != 1) {
+		client.say(messageInfo.channel, `@${messageInfo.user.username}, para te juntares à lista para jogar deves usar o comando da seguinte forma: !jogar <nick>`);
+	}
+	else {
+		client.say(messageInfo.channel, `@${messageInfo.user.username}, foste adicionado à lista para jogar! Nick: ${command.args[0]}! (Caso o nick não esteja correto contacta-me por favor)`);
+	}
+}
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const CHANNEL_NAME = 'TVMarvin';
+export const OAUTH_TOKEN = 'oauth:1oi3k8vyozh7i53xrcnnamdcbl1l4z';
+export const BOT_USERNAME = 'BazingaBot';
