ae7da968314569f66f54425e070e467ff2f2548a
Pedro Seixas <49153615+pedrojfs17@users.noreply.github.com>
2020-09-19 13:33:47 +0100

    Working bot

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const CHANNEL_NAME = 'TVMarvin';
-export const OAUTH_TOKEN = process.env.OAUTH_TOKEN || 'oauth:1oi3k8vyozh7i53xrcnnamdcbl1l4z';
+export const OAUTH_TOKEN = process.env.OAUTH_TOKEN;
 export const BOT_USERNAME = 'BazingaBot';
 
 export const SHEET = process.env.SHEET;
