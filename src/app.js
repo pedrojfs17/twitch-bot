2da3ba108a66a63123d851c998548dd54e839c69
Pedro Seixas <49153615+pedrojfs17@users.noreply.github.com>
2020-09-21 19:55:27 +0100

    Cleanup some code

--- src/app.js
+++ src/app.js
@@ -25,62 +25,9 @@ sheetsClient.authorize(function (err, tokens) {
 	readDatabase(sheetsClient, fullList);
 });
 
-/* async function readNumberOfEntries(cl) {
-	const gsapi = google.sheets({version:'v4', auth: cl});
-	const opt = {
-		spreadsheetId: SHEET,
-		range: 'Lista!E1'
-	};
-
-	let data = await gsapi.spreadsheets.values.get(opt);
-	console.log("Numero de entradas: " + data.data.values);
-	return data.data.values;
-} */
-
-/* async function readDatabase(cl) {
-	const nEntries = readNumberOfEntries(cl);
-	const gsapi = google.sheets({version:'v4', auth: cl});
-	const opt = {
-		spreadsheetId: SHEET,
-		range: 'Lista!A2:D' + (await nEntries + 1).toString()
-	};
-
-	let data = await gsapi.spreadsheets.values.get(opt);
-	fullList = data.data.values;
-	console.log(fullList);
-} */
-
-/* async function updateDatabase(cl) {
-	const gsapi = google.sheets({version:'v4', auth: cl});
-	const updateOpt = {
-		spreadsheetId: SHEET,
-		range: 'Lista!A2',
-		valueInputOption: 'USER_ENTERED',
-		resource: { values: fullList }
-	};
-
-	await gsapi.spreadsheets.values.update(updateOpt);
-} */
-
 export function addPlayerToList(data) {
 	fullList.push(data);
 }
-
-/* async function addPlayerToDatabase(data) {
-	//const data = [messageInfo.user['user-id'], messageInfo.user.username, nick, '=TIMESTAMP()'];
-	addPlayerToList(data);
-
-	const gsapi = google.sheets({version:'v4', auth: sheetsClient});
-
-	const updateOpt = {
-		spreadsheetId: SHEET,
-		range: 'Lista!A2',
-		valueInputOption: 'USER_ENTERED',
-		resource: { values: [data] }
-	};
-
-	await gsapi.spreadsheets.values.append(updateOpt);
-} */
 
 export function isPlayerInList(userID) {
 	for (var i = 0; i < fullList.length; i++) {
@@ -121,75 +68,3 @@ client.on('message', (channel, user, message, self) => {
 		callCommand(command, { channel, user, message });
 	}
 });
-
-/* let recognizeCommand = (message) => {
-	const regex = /\!(.*?)$/gm;
-	const fullCommand = regex.exec(message);
-  
-	if (fullCommand) {
-	  const splittedCommand = fullCommand[1].split(' ')
-	  const command = splittedCommand[0];
-  
-	  splittedCommand.shift() // remove command from array
-  
-	  return {
-		command: command,
-		args: splittedCommand
-	  }
-	}
-  
-	return false
-} */
-
-/* function callCommand(command, messageInfo) {
-	switch (command.command) {
-	  	case 'jogar':
-			playCommandHandler(command, messageInfo);
-			break
-	 	default:
-			break
-	}
-} */
-
-/* function playCommandHandler(command, messageInfo) {
-	// Add user to list with command.args[0] nickname
-	if (command.args.length != 1) {
-		client.say(messageInfo.channel, `@${messageInfo.user.username}, para te juntares à lista para jogar deves usar o comando da seguinte forma: !jogar <nick>`);
-	}
-	else if (!isPlayerInList(messageInfo.user['user-id'])) {	
-		const data = [messageInfo.user['user-id'], messageInfo.user.username, command.args[0], '=TIMESTAMP()'];
-		client.say(messageInfo.channel, `!watchtime @${messageInfo.user.username}`);
-		addPlayerToCheckList(messageInfo.user.username, data);
-	}
-	else {
-		client.say(messageInfo.channel, `@${messageInfo.user.username}, já te encontras na lista para jogar!`);
-	}
-} */
-
-/* function parseBotMessage(channel, message) {
-	const words = message.split(' ');
-	if (words[1] !== 'gastou') return;
-
-	for (var i = 0; i < playersToPlay.length; i++) {
-		if (playersToPlay[i][0] === words[0]) {
-			console.log('Found Player in List');
-			if (words[3] === 'days' || (Number(words[2]) >= 4 && words[3] === 'hours')) {
-				console.log('Player able to play');
-				addPlayerToDatabase(playersToPlay[i][1]);
-				client.say(channel, `@${playersToPlay[i][1]}, foste agora adicionado à lista para jogar com o nick: ${playersToPlay[i][2]}!`);
-			}
-			else
-				client.say(channel, `@${playersToPlay[i][0]}, necessitas de pelo menos 4 horas para estares apto para jogar!`);
-			playersToPlay.splice(i, 1);
-		}
-	}
-	console.log('Updated list: ');
-	console.log(playersToPlay);
-} */
-
-/* function addPlayerToCheckList(twitchUsername, data) {
-	let info = [twitchUsername, data];
-	playersToPlay.push(info);
-	console.log('Added player');
-	console.log(playersToPlay);
-} */
